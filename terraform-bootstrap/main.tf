3875aa2c5d3bbf973195715237373679361132de
Kalin Krastev <kkrastev@live.com>
2025-04-11 22:49:35 +0300

    Fixed LockID

--- terraform-bootstrap/main.tf
+++ terraform-bootstrap/main.tf
@@ -68,10 +68,10 @@ resource "aws_s3_bucket_policy" "example_bucket_policy" {
 resource "aws_dynamodb_table" "terraform_state_lock" {
   name           = "terraform-state-lock-${var.env}"
   billing_mode   = "PAY_PER_REQUEST" # On-demand pricing; change to PROVISIONED if needed
-  hash_key       = "LockId"          # Partition key column
+  hash_key       = "LockID"          # Partition key column
 
   attribute {
-    name = "LockId"
+    name = "LockID"
     type = "S" # "S" stands for String type
   }
 
