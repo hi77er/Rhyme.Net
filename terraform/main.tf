a18f5e307ccf77181bcdb4551d20670186b450dd
Kalin Krastev <kkrastev@live.com>
2025-04-11 15:28:42 +0300

    Added terraform destroy github workflow. Commented out everything except the DynamoDB module on terraform  Deploy workflow's main module.

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,28 +12,28 @@ provider "aws" {
   region = var.aws_region
 }
 
-module "lambdas" {
-  source                      = "./modules/lambdas"
-  aws_account_id              = var.aws_account_id
-  aws_region                  = var.aws_region
-  env                         = var.env
-  ecr_net_lambdas_repo_prefix = var.ecr_net_lambdas_repo_prefix
-  lambda_definitions = {
-    "new-order-lambda" = {
-      lambda_name = "new-order-lambda-${var.env}"
-      memory_size = 128
-      timeout = 60
-      filename = "../Rhyme.Net.Commands.NewOrder/publish/NewOrder.zip"
-      handler = "Rhyme.Net.Commands.NewOrder::Rhyme.Net.Commands.NewOrder.Function::FunctionHandler"
-      runtime = "dotnet8"
-    },
-  }
-}
+# module "lambdas" {
+#   source                      = "./modules/lambdas"
+#   aws_account_id              = var.aws_account_id
+#   aws_region                  = var.aws_region
+#   env                         = var.env
+#   ecr_net_lambdas_repo_prefix = var.ecr_net_lambdas_repo_prefix
+#   lambda_definitions = {
+#     "new-order-lambda" = {
+#       lambda_name = "new-order-lambda-${var.env}"
+#       memory_size = 128
+#       timeout = 60
+#       filename = "../Rhyme.Net.Commands.NewOrder/publish/NewOrder.zip"
+#       handler = "Rhyme.Net.Commands.NewOrder::Rhyme.Net.Commands.NewOrder.Function::FunctionHandler"
+#       runtime = "dotnet8"
+#     },
+#   }
+# }
 
-module "ecr" {
-  source = "./modules/ecr"
-  environment = var.environment
-}
+# module "ecr" {
+#   source = "./modules/ecr"
+#   environment = var.environment
+# }
 
 module "dynamodb" {
   source = "./modules/dynamodb"
