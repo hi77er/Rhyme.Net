15f3415d57dc864d040b6ee8d8d8889b1dbbed91
Kalin Krastev <kkrastev@live.com>
2025-04-30 17:41:42 +0300

    Added readytorun attribute while building

--- Rhyme.Net.Commands.NewOrderLambda/src/Rhyme.Net.Commands.NewOrderLambda/Function.cs
+++ Rhyme.Net.Commands.NewOrderLambda/src/Rhyme.Net.Commands.NewOrderLambda/Function.cs
@@ -16,7 +16,7 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
-        Console.WriteLine($"Input: 10");
+        Console.WriteLine($"Input: 11");
         return input.ToUpper();
     }
 }
