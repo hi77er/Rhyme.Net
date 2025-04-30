2fb4f286caca9aa84a8190eb5ffcf8c474467a1e
Kalin Krastev <kkrastev@live.com>
2025-04-30 16:41:31 +0300

    Changed labmdas

--- Rhyme.Net.Queries.GetOrdersLambda/src/Rhyme.Net.Queries.GetOrdersLambda/Function.cs
+++ Rhyme.Net.Queries.GetOrdersLambda/src/Rhyme.Net.Queries.GetOrdersLambda/Function.cs
@@ -16,7 +16,7 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
-        Console.WriteLine($"Input: 3");
+        Console.WriteLine($"Input: 4");
         return input.ToUpper();
     }
 }
