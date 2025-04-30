49653f4489df0735bcf5831fd4e7c2883d7ab1b3
Kalin Krastev <kkrastev@live.com>
2025-04-30 17:50:51 +0300

    Fixed publish command

--- Rhyme.Net.Queries.GetOrdersLambda/src/Rhyme.Net.Queries.GetOrdersLambda/Function.cs
+++ Rhyme.Net.Queries.GetOrdersLambda/src/Rhyme.Net.Queries.GetOrdersLambda/Function.cs
@@ -16,7 +16,7 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
-        Console.WriteLine($"Input: 11");
+        Console.WriteLine($"Input: 12");
         return input.ToUpper();
     }
 }
