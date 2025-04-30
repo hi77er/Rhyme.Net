c6021a9f1d66731b2e24b9a365ab8ade0b5b7203
Kalin Krastev <kkrastev@live.com>
2025-04-30 16:45:07 +0300

    Updated lambdas

--- Rhyme.Net.Commands.NewOrderLambda/src/Rhyme.Net.Commands.NewOrderLambda/Function.cs
+++ Rhyme.Net.Commands.NewOrderLambda/src/Rhyme.Net.Commands.NewOrderLambda/Function.cs
@@ -16,7 +16,7 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
-        Console.WriteLine($"Input: 4");
+        Console.WriteLine($"Input: 5");
         return input.ToUpper();
     }
 }
