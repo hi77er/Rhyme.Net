0340cfb68d5709e2381dd806481eadc8941a3d85
Kalin Krastev <kkrastev@live.com>
2025-05-07 14:44:07 +0300

    Splitted project name by dots

--- Rhyme.Net.Queries.GetOrdersLambda/src/Rhyme.Net.Queries.GetOrdersLambda/Function.cs
+++ Rhyme.Net.Queries.GetOrdersLambda/src/Rhyme.Net.Queries.GetOrdersLambda/Function.cs
@@ -16,7 +16,7 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
-        Console.WriteLine($"Input: 16");
+        Console.WriteLine($"Input: 17");
         return input.ToUpper();
     }
 }
