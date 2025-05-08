77deac0105217eb22e1febe58939f127ca6c7241
Kalin Krastev <kkrastev@live.com>
2025-05-08 16:52:02 +0300

    Added public exposure to the api gateway

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -3,10 +3,47 @@ resource "aws_api_gateway_rest_api" "orders_api" {
   description = "API Gateway for orders service"
 }
 
+resource "aws_api_gateway_rest_api_policy" "orders_api_policy" {
+  rest_api_id = aws_api_gateway_rest_api.orders_api.id
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "execute-api:Invoke"
+        Resource  = "${aws_api_gateway_rest_api.orders_api.execution_arn}/*/*"
+      }
+    ]
+  })
+}
+
 resource "aws_api_gateway_resource" "orders" {
   rest_api_id = aws_api_gateway_rest_api.orders_api.id
   parent_id   = aws_api_gateway_rest_api.orders_api.root_resource_id
   path_part   = "orders"
+}
+
+resource "aws_api_gateway_method_response" "cors" {
+  rest_api_id = aws_api_gateway_rest_api.orders_api.id
+  resource_id = aws_api_gateway_resource.orders.id
+  http_method = "OPTIONS"
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+  }
+}
+
+resource "aws_api_gateway_deployment" "orders_api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.orders_api.id
+}
+
+resource "aws_api_gateway_stage" "orders_api_stage" {
+  rest_api_id   = aws_api_gateway_rest_api.orders_api.id
+  deployment_id = aws_api_gateway_deployment.orders_api_deployment.id
+  stage_name    = var.env
 }
 
 resource "aws_api_gateway_method" "orders_method" {
