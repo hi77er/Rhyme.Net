5a67c900aff031295286d0e352c253f18420f4c2
Kalin Krastev <kkrastev@live.com>
2025-05-08 17:04:25 +0300

    Moved cors to last

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -24,18 +24,6 @@ resource "aws_api_gateway_resource" "orders" {
   path_part   = "orders"
 }
 
-resource "aws_api_gateway_method_response" "cors" {
-  rest_api_id = aws_api_gateway_rest_api.orders_api.id
-  resource_id = aws_api_gateway_resource.orders.id
-  http_method = "OPTIONS"
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin"  = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-  }
-}
-
 resource "aws_api_gateway_deployment" "orders_api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.orders_api.id
 }
@@ -64,4 +52,16 @@ resource "aws_api_gateway_integration" "orders_integration" {
   integration_http_method = each.value.http_method
   type        = "AWS_PROXY"
   uri         = var.api_gateway_lambda_invoke_arns[each.key]
+}
+
+resource "aws_api_gateway_method_response" "cors" {
+  rest_api_id = aws_api_gateway_rest_api.orders_api.id
+  resource_id = aws_api_gateway_resource.orders.id
+  http_method = "OPTIONS"
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+  }
 }
