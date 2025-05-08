75e3d0bbce407957ce3ea94e8962e7f5c0c4bf38
Kalin Krastev <kkrastev@live.com>
2025-05-08 15:08:55 +0300

    Added integration_http_method

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -24,6 +24,7 @@ resource "aws_api_gateway_integration" "orders_integration" {
   rest_api_id = aws_api_gateway_rest_api.orders_api.id
   resource_id = aws_api_gateway_resource.orders.id
   http_method = aws_api_gateway_method.orders_method[each.key].http_method
+  integration_http_method = each.value.http_method
   type        = "AWS_PROXY"
   uri         = var.api_gateway_lambda_invoke_arns[each.key]
 }
