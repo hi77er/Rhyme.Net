f8bb37dfecfda18cbff05bb85164cf044fbd5538
Kalin Krastev <kkrastev@live.com>
2025-05-08 18:29:10 +0300

    Moved the zip location

--- Rhyme.Net.Commands.NewOrderLambda/src/Rhyme.Net.Commands.NewOrderLambda/Function.cs
+++ Rhyme.Net.Commands.NewOrderLambda/src/Rhyme.Net.Commands.NewOrderLambda/Function.cs
@@ -16,7 +16,7 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
-        Console.WriteLine($"Input: 26");
+        Console.WriteLine($"Input: 27");
         return input.ToUpper();
     }
 }
