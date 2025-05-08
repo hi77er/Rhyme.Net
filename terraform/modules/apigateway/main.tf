37a7e172cd5e1290db8b05a648b1ee5b07a45373
Kalin Krastev <kkrastev@live.com>
2025-05-08 15:02:28 +0300

    Http method try fix

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -11,7 +11,7 @@ resource "aws_api_gateway_resource" "orders" {
 
 resource "aws_api_gateway_method" "orders_method" {
   for_each      = var.api_gateway_lambda_definitions
-
+  
   rest_api_id   = aws_api_gateway_rest_api.orders_api.id
   resource_id   = aws_api_gateway_resource.orders.id
   http_method   = each.value.http_method
@@ -23,7 +23,7 @@ resource "aws_api_gateway_integration" "orders_integration" {
 
   rest_api_id = aws_api_gateway_rest_api.orders_api.id
   resource_id = aws_api_gateway_resource.orders.id
-  http_method = each.value.http_method
+  http_method = aws_api_gateway_method.orders_method[each.key].http_method
   type        = "AWS_PROXY"
   uri         = var.api_gateway_lambda_invoke_arns[each.key]
 }
