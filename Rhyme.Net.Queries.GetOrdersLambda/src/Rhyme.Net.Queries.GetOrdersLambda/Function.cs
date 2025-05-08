136631cd50f1e3a56cc93ea231b1e01ab53ee0d6
Kalin Krastev <kkrastev@live.com>
2025-05-08 20:08:32 +0300

    Changed lambda types. Added reset initial dir to sctipt.

--- Rhyme.Net.Queries.GetOrdersLambda/src/Rhyme.Net.Queries.GetOrdersLambda/Function.cs
+++ Rhyme.Net.Queries.GetOrdersLambda/src/Rhyme.Net.Queries.GetOrdersLambda/Function.cs
@@ -1,23 +1,35 @@
 using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using System.Text.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+[assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
 
 namespace Rhyme.Net.Queries.GetOrdersLambda;
 
 public class Function
 {
-    
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
-    /// <param name="input">The event for the Lambda function handler to process.</param>
+    /// <param name="request">The API gateway request object for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public APIGatewayHttpApiV2ProxyResponse FunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
     {
-        Console.WriteLine($"Lambda v36");
-        Console.WriteLine($"Input: {input}");
-        return input.ToUpper();
+        Console.WriteLine($"Lambda v37");
+
+        context.Logger.LogLine($"Received request: {JsonSerializer.Serialize(request)}");
+
+        // Read request body
+        string requestBody = request.Body ?? "No Body Provided";
+
+        return new APIGatewayHttpApiV2ProxyResponse
+        {
+            StatusCode = 200,
+            Body = JsonSerializer.Serialize(new { message = $"Order ID: test", body = requestBody }),
+            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+        };
     }
 }
