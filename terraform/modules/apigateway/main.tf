98e14ba3bf9daa49da4582be763765ad64f54e9c
Kalin Krastev <kkrastev@live.com>
2025-05-11 15:20:36 +0300

    Adding cloudwatch policy to apgw

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -3,53 +3,54 @@ resource "aws_api_gateway_rest_api" "orders_api" {
   description = "API Gateway for orders service"
 }
 
-# resource "aws_iam_role" "apigateway_logging_role" {
-#   name = "APIGatewayLoggingRole"
+resource "aws_iam_role" "apigateway_logging_role" {
+  name = "APIGatewayLoggingRole"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "apigateway.amazonaws.com"
-#         }
-#         Action = "sts:AssumeRole"
-#       }
-#     ]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "apigateway.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_policy" "apigateway_logging_policy" {
-#   name        = "APIGatewayLoggingPolicy"
-#   description = "Policy to allow API Gateway logging to CloudWatch"
+resource "aws_iam_policy" "apigateway_logging_policy" {
+  name        = "APIGatewayLoggingPolicy"
+  description = "Policy to allow API Gateway logging to CloudWatch"
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Action = [
-#           "logs:CreateLogGroup",
-#           "logs:CreateLogStream",
-#           "logs:DescribeLogGroups",
-#           "logs:DescribeLogStreams",
-#           "logs:PutLogEvents"
-#         ]
-#         Resource = "arn:aws:logs:${var.aws_region}:${var.aws_account_id}:log-group:/aws/apigateway/orders-api:*"
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:${var.aws_region}:${var.aws_account_id}:log-group:/aws/apigateway/orders-api:*"
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "attach_logging_policy" {
-#   role       = aws_iam_role.apigateway_logging_role.name
-#   policy_arn = aws_iam_policy.apigateway_logging_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "attach_logging_policy" {
+  role       = aws_iam_role.apigateway_logging_role.name
+  policy_arn = aws_iam_policy.apigateway_logging_policy.arn
+}
 
-# resource "aws_api_gateway_account" "gateway_account_settings" {
-#   cloudwatch_role_arn = aws_iam_role.apigateway_logging_role.arn
-# }
+resource "aws_api_gateway_account" "gateway_account_settings" {
+  cloudwatch_role_arn = aws_iam_role.apigateway_logging_role.arn
+  depends_on          = [aws_iam_role_policy_attachment.attach_logging_policy]
+}
 
 resource "aws_api_gateway_rest_api_policy" "orders_api_policy" {
   rest_api_id = aws_api_gateway_rest_api.orders_api.id
@@ -102,7 +103,7 @@ resource "aws_api_gateway_method_response" "options_response" {
   resource_id = aws_api_gateway_resource.orders.id
   http_method = "OPTIONS"
   status_code = "200"
-  depends_on = [aws_api_gateway_method.options_method]
+  depends_on  = [aws_api_gateway_method.options_method]
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin"  = true
@@ -131,7 +132,7 @@ resource "aws_api_gateway_integration" "options_integration" {
 resource "aws_api_gateway_integration_response" "options_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.orders_api.id
   resource_id = aws_api_gateway_resource.orders.id
-  depends_on = [aws_api_gateway_integration.options_integration]
+  depends_on  = [aws_api_gateway_integration.options_integration]
   http_method = "OPTIONS"
   status_code = "200"
 
