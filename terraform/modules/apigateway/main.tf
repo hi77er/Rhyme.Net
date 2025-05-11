bae01dfa8f7b3329eeb9de723a42922492cc459f
Kalin Krastev <kkrastev@live.com>
2025-05-11 15:53:19 +0300

    Added custom policy

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -25,40 +25,35 @@ resource "aws_iam_role" "apigateway_logging_role" {
   })
 }
 
-# resource "aws_iam_policy" "apigateway_logging_policy" {
-#   name        = "APIGatewayLoggingPolicy"
-#   description = "Policy to allow API Gateway logging to CloudWatch"
-#   depends_on = [ aws_iam_role.apigateway_logging_role ]
+resource "aws_iam_policy" "apigateway_logging_policy" {
+  name        = "APIGatewayLoggingPolicy"
+  description = "Policy to allow API Gateway logging to CloudWatch"
+  depends_on = [ aws_iam_role.apigateway_logging_role ]
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Action = [
-#           "logs:CreateLogGroup",
-#           "logs:CreateLogStream",
-#           "logs:DescribeLogGroups",
-#           "logs:DescribeLogStreams",
-#           "logs:PutLogEvents"
-#         ]
-#         Resource = "arn:aws:logs:${var.aws_region}:${var.aws_account_id}:log-group:/aws/apigateway/orders-api:*"
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:log-group:/aws/api-gateway/*"
+      }
+    ]
+  })
+}
 
 resource "aws_iam_policy_attachment" "api_gw_logs_attachment" {
   name       = "APIGatewayLogsPolicyAttachment"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSApiGatewayPushToCloudWatchLogs"
+  policy_arn = aws_iam_policy.apigateway_logging_policy.arn
   roles      = [aws_iam_role.apigateway_logging_role.name]
-}
 
-# resource "aws_iam_role_policy_attachment" "attach_logging_policy" {
-#   role       = aws_iam_role.apigateway_logging_role.name
-#   policy_arn = aws_iam_policy.apigateway_logging_policy.arn
-#   depends_on = [aws_iam_policy.apigateway_logging_policy]
-# }
+}
 
 resource "aws_api_gateway_account" "gateway_account_settings" {
   cloudwatch_role_arn = aws_iam_role.apigateway_logging_role.arn
