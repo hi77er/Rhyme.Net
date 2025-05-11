740817dc6524039fa7206ba02b11a69265dbf0b1
Kalin Krastev <kkrastev@live.com>
2025-05-11 13:54:35 +0300

    Removed logging temporarrely

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -3,53 +3,53 @@ resource "aws_api_gateway_rest_api" "orders_api" {
   description = "API Gateway for orders service"
 }
 
-resource "aws_iam_role" "apigateway_logging_role" {
-  name = "APIGatewayLoggingRole"
+# resource "aws_iam_role" "apigateway_logging_role" {
+#   name = "APIGatewayLoggingRole"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "apigateway.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = {
+#           Service = "apigateway.amazonaws.com"
+#         }
+#         Action = "sts:AssumeRole"
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_policy" "apigateway_logging_policy" {
-  name        = "APIGatewayLoggingPolicy"
-  description = "Policy to allow API Gateway logging to CloudWatch"
+# resource "aws_iam_policy" "apigateway_logging_policy" {
+#   name        = "APIGatewayLoggingPolicy"
+#   description = "Policy to allow API Gateway logging to CloudWatch"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:DescribeLogGroups",
-          "logs:DescribeLogStreams",
-          "logs:PutLogEvents"
-        ]
-        Resource = "arn:aws:logs:${var.aws_region}:${var.aws_account_id}:log-group:/aws/apigateway/orders-api:*"
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "logs:CreateLogGroup",
+#           "logs:CreateLogStream",
+#           "logs:DescribeLogGroups",
+#           "logs:DescribeLogStreams",
+#           "logs:PutLogEvents"
+#         ]
+#         Resource = "arn:aws:logs:${var.aws_region}:${var.aws_account_id}:log-group:/aws/apigateway/orders-api:*"
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "attach_logging_policy" {
-  role       = aws_iam_role.apigateway_logging_role.name
-  policy_arn = aws_iam_policy.apigateway_logging_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "attach_logging_policy" {
+#   role       = aws_iam_role.apigateway_logging_role.name
+#   policy_arn = aws_iam_policy.apigateway_logging_policy.arn
+# }
 
-resource "aws_api_gateway_account" "gateway_account_settings" {
-  cloudwatch_role_arn = aws_iam_role.apigateway_logging_role.arn
-}
+# resource "aws_api_gateway_account" "gateway_account_settings" {
+#   cloudwatch_role_arn = aws_iam_role.apigateway_logging_role.arn
+# }
 
 resource "aws_api_gateway_rest_api_policy" "orders_api_policy" {
   rest_api_id = aws_api_gateway_rest_api.orders_api.id
