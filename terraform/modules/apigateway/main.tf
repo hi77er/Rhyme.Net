6b26ec0fd39a291aeabbfdc08b6571de9e8d8f65
Kalin Krastev <kkrastev@live.com>
2025-05-11 13:22:43 +0300

    Added cors for integrations response

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -70,6 +70,26 @@ resource "aws_api_gateway_method_response" "orders_cors_response" {
   }
 }
 
+resource "aws_api_gateway_integration_response" "get_integration_response" {
+  for_each = var.api_gateway_lambda_definitions
+
+  rest_api_id = aws_api_gateway_rest_api.orders_api.id
+  resource_id = aws_api_gateway_resource.orders.id
+  depends_on  = [
+    aws_api_gateway_method.orders_method, 
+    aws_api_gateway_integration.orders_integration
+  ]
+
+  http_method = each.value.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET,POST,PUT,DELETE'"
+  }
+}
+
 resource "aws_api_gateway_integration" "orders_integration" {
   for_each = var.api_gateway_lambda_definitions
 
