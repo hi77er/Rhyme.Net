2fd9bb616920a969ab5f2fd0aea0a490e522a7d5
Kalin Krastev <kkrastev@live.com>
2025-05-14 17:34:24 +0300

    Fixed lambda arns output

--- terraform/modules/lambdas/outputs.tf
+++ terraform/modules/lambdas/outputs.tf
@@ -1,3 +1,3 @@
 output "api_gateway_lambda_invoke_arns" {
-  value = { for k, v in aws_lambda_function.lambda : k => v.invoke_arn }
+  value = { for k, v in aws_lambda_function.apigateway_lambdas : k => v.invoke_arn }
 }
