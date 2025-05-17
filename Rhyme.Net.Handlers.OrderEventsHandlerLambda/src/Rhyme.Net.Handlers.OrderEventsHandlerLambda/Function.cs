3f53788b2a4bac02bf4d180287e1b25415f779d7
Kalin Krastev <kkrastev@live.com>
2025-05-17 13:57:19 +0300

    Added sample code to the hanlder lambda

--- Rhyme.Net.Handlers.OrderEventsHandlerLambda/src/Rhyme.Net.Handlers.OrderEventsHandlerLambda/Function.cs
+++ Rhyme.Net.Handlers.OrderEventsHandlerLambda/src/Rhyme.Net.Handlers.OrderEventsHandlerLambda/Function.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using Amazon.Lambda.DynamoDBEvents;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -7,15 +8,26 @@ namespace Rhyme.Net.Handlers.OrderEventsHandlerLambda;
 
 public class Function
 {
-    
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
-    /// <param name="input">The event for the Lambda function handler to process.</param>
+    /// <param name="dynamoDbEvent">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public void FunctionHandler(DynamoDBEvent dynamoDbEvent, ILambdaContext context)
     {
-        return input.ToUpper();
+        Console.WriteLine($"Processing {dynamoDbEvent.Records.Count} records...");
+
+        foreach (var record in dynamoDbEvent.Records)
+        {
+            if (record.EventName == "INSERT") // Check if it's a create event
+            {
+                Console.WriteLine($"New item added with Event ID: {record.EventID}");
+
+            }
+        }
+
+        Console.WriteLine("Stream processing complete.");
     }
 }
