a6f12d327667940a3ba5c6c9751a450b6dba0e57
Kalin Krastev <kkrastev@live.com>
2025-06-27 17:06:09 +0300

    Depl prep

--- Rhyme.Net.Queries.GetOrdersLambda/src/Rhyme.Net.Queries.GetOrdersLambda/Function.cs
+++ Rhyme.Net.Queries.GetOrdersLambda/src/Rhyme.Net.Queries.GetOrdersLambda/Function.cs
@@ -18,7 +18,7 @@ public class Function
     /// <returns></returns>
     public APIGatewayHttpApiV2ProxyResponse FunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
     {
-        Console.WriteLine($"Lambda v52");
+        Console.WriteLine($"Lambda v54");
 
         context.Logger.LogLine($"Received request: {JsonSerializer.Serialize(request)}");
 
