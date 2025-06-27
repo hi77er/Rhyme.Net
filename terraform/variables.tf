a4314322f2dd8b2e443cdf4f11e8b4b1258cf6a6
Kalin Krastev <kkrastev@live.com>
2025-06-28 02:56:57 +0300

    mem up

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -67,8 +67,8 @@ variable "api_gateway_lambda_definitions" {
     }
     "CouponsForCampaignLambda-dev" = {
       lambda_name   = "CouponsForCampaignLambda-dev"
-      memory_size   = 128
-      timeout       = 60
+      memory_size   = 256
+      timeout       = 360
       handler       = "Rhyme.Net.Commands.CouponsForCampaignLambda::Rhyme.Net.Commands.CouponsForCampaignLambda.Function::FunctionHandler"
       runtime       = "dotnet8"
       http_method   = "POST"
