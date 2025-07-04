54c018472dd116b3b9f0d5434909b0b4a13310e3
Kalin Krastev <kkrastev@live.com>
2025-07-04 19:09:23 +0300

    Commented out job code implementation

--- Rhyme.Net.CouponsForCampaignJob/Program.cs
+++ Rhyme.Net.CouponsForCampaignJob/Program.cs
@@ -11,65 +11,65 @@ using Rhyme.Net.UseCases.Coupons.GenerateForCampaign;
 
 public class Program
 {
-  private static IServiceProvider? _serviceProvider;
-  private static GenerateForCampaignHandler? _handler;
+  // private static IServiceProvider? _serviceProvider;
+  // private static GenerateForCampaignHandler? _handler;
 
   static void Main(string[] args)
   {
-    Console.WriteLine($"HANDLER: Lambda v58");
+    Console.WriteLine($"HANDLER: Lambda v60");
 
-    var request = MapRequest(args);
-    Console.WriteLine($"HANDLER: Received request: {JsonSerializer.Serialize(request)}");
+    // var request = MapRequest(args);
+    // Console.WriteLine($"HANDLER: Received request: {JsonSerializer.Serialize(request)}");
 
-    Guard.Against.Null(request, nameof(request));
-    var command = new GenerateForCampaignCommand(request.CampaignId, request.TotalCouponsCount);
+    // Guard.Against.Null(request, nameof(request));
+    // var command = new GenerateForCampaignCommand(request.CampaignId, request.TotalCouponsCount);
 
-    _serviceProvider = ConfigureServices();
-    _handler = _serviceProvider.GetRequiredService<GenerateForCampaignHandler>();
+    // _serviceProvider = ConfigureServices();
+    // _handler = _serviceProvider.GetRequiredService<GenerateForCampaignHandler>();
 
-    var result = _handler.Handle(command, CancellationToken.None).Result;
-    Console.WriteLine($"HANDLER: Success = {result.IsSuccess};");
+    // var result = _handler.Handle(command, CancellationToken.None).Result;
+    // Console.WriteLine($"HANDLER: Success = {result.IsSuccess};");
     // var response = new CouponsForCampaignResponse(result);
   }
 
-  private static IServiceProvider ConfigureServices()
-  {
-    var serviceProvider = new ServiceCollection()
-        .AddSingleton<IAmazonDynamoDB>(_ =>
-        {
-          var region = Environment.GetEnvironmentVariable("DYNAMODB_REGION") ?? "eu-central-1";
-          var client = new AmazonDynamoDBClient(Amazon.RegionEndpoint.GetBySystemName(region));
-          return client;
-        })
-        .AddSingleton<IDynamoDBContext>(_ =>
-        {
-          var region = Environment.GetEnvironmentVariable("DYNAMODB_REGION") ?? "eu-central-1";
-          var client = new AmazonDynamoDBClient(Amazon.RegionEndpoint.GetBySystemName(region));
-          return new DynamoDBContext(client);
-        })
-        .AddSingleton<IDynamoRepository<Coupon, string>, DynamoRepository<Coupon, string>>()
-        .AddSingleton<IGenerateCampaignCouponsService, GenerateCampaignCouponsService>()
-        .AddSingleton<GenerateForCampaignHandler>()
-        .BuildServiceProvider();
+  // private static IServiceProvider ConfigureServices()
+  // {
+  //   var serviceProvider = new ServiceCollection()
+  //       .AddSingleton<IAmazonDynamoDB>(_ =>
+  //       {
+  //         var region = Environment.GetEnvironmentVariable("DYNAMODB_REGION") ?? "eu-central-1";
+  //         var client = new AmazonDynamoDBClient(Amazon.RegionEndpoint.GetBySystemName(region));
+  //         return client;
+  //       })
+  //       .AddSingleton<IDynamoDBContext>(_ =>
+  //       {
+  //         var region = Environment.GetEnvironmentVariable("DYNAMODB_REGION") ?? "eu-central-1";
+  //         var client = new AmazonDynamoDBClient(Amazon.RegionEndpoint.GetBySystemName(region));
+  //         return new DynamoDBContext(client);
+  //       })
+  //       .AddSingleton<IDynamoRepository<Coupon, string>, DynamoRepository<Coupon, string>>()
+  //       .AddSingleton<IGenerateCampaignCouponsService, GenerateCampaignCouponsService>()
+  //       .AddSingleton<GenerateForCampaignHandler>()
+  //       .BuildServiceProvider();
 
-    return serviceProvider;
-  }
+  //   return serviceProvider;
+  // }
 
-  private static CouponsForCampaignRequestBody? MapRequest(string[] args)
-  {
-    if (args.Length < 2)
-    {
-      Console.WriteLine("Usage: <CampaignId> <TotalCouponsCount>");
-      return null;
-    }
+  // private static CouponsForCampaignRequestBody? MapRequest(string[] args)
+  // {
+  //   if (args.Length < 2)
+  //   {
+  //     Console.WriteLine("Usage: <CampaignId> <TotalCouponsCount>");
+  //     return null;
+  //   }
 
-    var campaignId = args[0];
-    if (!int.TryParse(args[1], out var totalCouponsCount))
-    {
-      Console.WriteLine("Invalid TotalCouponsCount argument.");
-      return null;
-    }
+  //   var campaignId = args[0];
+  //   if (!int.TryParse(args[1], out var totalCouponsCount))
+  //   {
+  //     Console.WriteLine("Invalid TotalCouponsCount argument.");
+  //     return null;
+  //   }
 
-    return new CouponsForCampaignRequestBody(campaignId, totalCouponsCount);
-  }
+  //   return new CouponsForCampaignRequestBody(campaignId, totalCouponsCount);
+  // }
 }
