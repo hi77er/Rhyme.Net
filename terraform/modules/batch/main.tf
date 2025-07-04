3827e754f9fd71a48785698ba05f7d1084a60545
Kalin Krastev <kkrastev@live.com>
2025-07-04 19:26:42 +0300

    Updated batch security group

--- terraform/modules/batch/main.tf
+++ terraform/modules/batch/main.tf
@@ -13,7 +13,7 @@ data "aws_iam_policy_document" "batch_policy" {
 }
 
 resource "aws_iam_role" "batch_role" {
-  name = "aws-batch-service-role-${var.env}"
+  name               = "aws-batch-service-role-${var.env}"
   assume_role_policy = data.aws_iam_policy_document.batch_policy.json
 }
 
@@ -33,7 +33,7 @@ data "aws_iam_policy_document" "ecs_task_policy" {
 }
 
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole-${var.env}"
+  name               = "ecsTaskExecutionRole-${var.env}"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_policy.json
 }
 
@@ -45,7 +45,14 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy_attachment"
 resource "aws_security_group" "batch_security_group" {
   name        = "batch-fargate-sg-${var.env}"
   description = "Allow all egress"
-  vpc_id      = "<YOUR_VPC_ID>"
+  vpc_id      = "vpc-0c368a4f2b4097c4d"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
@@ -58,37 +65,37 @@ resource "aws_security_group" "batch_security_group" {
 resource "aws_batch_compute_environment" "coupon_generation_fargate_env" {
   compute_environment_name = "coupon-generation-fargate-ce-${var.env}"
   compute_resources {
-    max_vcpus           = 16
-    subnets             = ["<YOUR_SUBNET_ID_1>", "<YOUR_SUBNET_ID_2>"]
-    security_group_ids  = [aws_security_group.batch_security_group.id]
-    type                = "FARGATE"
+    max_vcpus          = 16
+    subnets            = ["<YOUR_SUBNET_ID_1>", "<YOUR_SUBNET_ID_2>"]
+    security_group_ids = [aws_security_group.batch_security_group.id]
+    type               = "FARGATE"
   }
   service_role = aws_iam_role.batch_role.arn
   type         = "MANAGED"
 }
 
 resource "aws_batch_job_queue" "coupon_generatio_job_queue" {
-  name                 = "coupon-generatio-job-queue"
-  state                = "ENABLED"
-  priority             = 1
+  name     = "coupon-generatio-job-queue"
+  state    = "ENABLED"
+  priority = 1
 
   compute_environment_order {
-    order                  = 1
-    compute_environment    = aws_batch_compute_environment.coupon_generation_fargate_env.arn
+    order               = 1
+    compute_environment = aws_batch_compute_environment.coupon_generation_fargate_env.arn
   }
 }
 
 resource "aws_batch_job_definition" "coupon_generation_job_def" {
-  for_each  = var.batch_job_definitions
-  name      = "${each.value.job_name}-def"
-  type      = "container"
+  for_each = var.batch_job_definitions
+  name     = "${each.value.job_name}-def"
+  type     = "container"
 
   container_properties = jsonencode({
-    image      = "${aws_ecr_repository.batch_jobs_repo.repository_url}:${each.value.job_name}"
-    vcpus      = 1
-    memory     = 1024
-    command    = []
-    jobRoleArn = aws_iam_role.ecs_task_execution_role.arn
+    image            = "${aws_ecr_repository.batch_jobs_repo.repository_url}:${each.value.job_name}"
+    vcpus            = 1
+    memory           = 1024
+    command          = []
+    jobRoleArn       = aws_iam_role.ecs_task_execution_role.arn
     executionRoleArn = aws_iam_role.ecs_task_execution_role.arn
     networkConfiguration = {
       assignPublicIp = "ENABLED"
