6541ccfbc63460ac5e4b3b6a70d93ac3fdff8e5e
Kalin Krastev <kkrastev@live.com>
2025-07-08 01:00:25 +0300

    removed policy

--- terraform/modules/batch/main.tf
+++ terraform/modules/batch/main.tf
@@ -26,12 +26,6 @@ resource "aws_iam_role_policy_attachment" "batch_full_access_attachment" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_service_role_policy_attachment" {
-  depends_on = [aws_iam_role.batch_role]
-  role       = aws_iam_role.batch_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-}
-
 resource "aws_iam_role_policy" "ecs_cluster_management_policy" {
   depends_on = [aws_iam_role.batch_role]
   name       = "ecs-cluster-management-policy-${var.env}"
