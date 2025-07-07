b12e5a8b3badd06f8f9df870d00f7d5bf2872c41
Kalin Krastev <kkrastev@live.com>
2025-07-07 23:58:43 +0300

    Added more dependencies

--- terraform/modules/batch/main.tf
+++ terraform/modules/batch/main.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "batch_policy" {
 }
 
 resource "aws_iam_role" "batch_role" {
-  depends_on = [ data.aws_iam_policy_document.batch_policy ]
+  depends_on         = [data.aws_iam_policy_document.batch_policy]
   name               = "aws-batch-service-role-${var.env}"
   assume_role_policy = data.aws_iam_policy_document.batch_policy.json
 }
@@ -32,47 +32,6 @@ resource "aws_iam_role_policy_attachment" "ecs_full_access_attachment" {
 resource "aws_ecr_repository" "batch_jobs_repo" {
   name = "batch-jobs-repo-${var.env}"
 }
-
-# data "aws_iam_policy_document" "batch_policy" {
-#   statement {
-#     actions = ["sts:AssumeRole"]
-#     principals {
-#       type        = "Service"
-#       identifiers = ["batch.amazonaws.com"]
-#     }
-#   }
-# }
-
-# resource "aws_iam_role" "batch_role" {
-#   name               = "aws-batch-service-role-${var.env}"
-#   assume_role_policy = data.aws_iam_policy_document.batch_policy.json
-# }
-
-# resource "aws_iam_role_policy_attachment" "batch_policy_attachment" {
-#   depends_on = [ aws_iam_role.batch_role ]
-#   role       = aws_iam_role.batch_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
-# }
-
-# resource "aws_iam_role_policy" "ecs_delete_cluster_policy" {
-#   name = "allow-ecs-delete-cluster"
-#   depends_on = [ aws_iam_role.batch_role ]
-#   role = aws_iam_role.batch_role.id
-
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Effect = "Allow",
-#         Action = [
-#           "ecs:DeleteCluster",
-#           "ecs:ListClusters"
-#         ],
-#         Resource = "arn:aws:ecs:eu-central-1:${data.aws_caller_identity.current.account_id}:cluster/*"
-#       }
-#     ]
-#   })
-# }
 
 data "aws_iam_policy_document" "ecs_task_policy" {
   statement {
@@ -116,7 +75,14 @@ resource "aws_security_group" "batch_security_group" {
 
 resource "aws_batch_compute_environment" "coupon_generation_fargate_env" {
   compute_environment_name = "coupon-generation-fargate-ce-${var.env}"
-  depends_on               = [aws_security_group.batch_security_group]
+  depends_on = [
+    aws_security_group.batch_security_group,
+    aws_iam_role.batch_role,
+    aws_iam_role.ecs_task_execution_role,
+    aws_iam_role_policy_attachment.ecs_task_execution_policy_attachment,
+    aws_iam_role_policy_attachment.batch_full_access_attachment,
+    aws_iam_role_policy_attachment.ecs_full_access_attachment,
+  ]
   compute_resources {
     max_vcpus          = 16
     subnets            = ["subnet-0a420baeba3391c2f", "subnet-0d3dfd7e7e2b58ed4"]
