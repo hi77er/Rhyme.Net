38e99534c808f7aa47ddcb5c4ebffc98847e1743
Kalin Krastev <kkrastev@live.com>
2025-07-08 13:57:43 +0300

    fixed vcpus

--- terraform/modules/batch/main.tf
+++ terraform/modules/batch/main.tf
@@ -138,11 +138,11 @@ resource "aws_batch_job_definition" "coupon_generation_job_def" {
     resourceRequirements = [
       {
         type  = "VCPU"
-        value = "1"
+        value = "0.5"
       },
       {
         type  = "MEMORY"
-        value = "512"
+        value = "1024"
       }
     ]
     command              = []
